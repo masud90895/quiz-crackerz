ae55e97fe1a378bca89f5737cdc9fb50177044a7
Md Mahafujur Rahaman Masud <masudhossainmbs129@gmail.com>
2022-10-10 21:06:14 +0600

    header component added

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header/Header';
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Header></Header>,
+    }
+  ])
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
