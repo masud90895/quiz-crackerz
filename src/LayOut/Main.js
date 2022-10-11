977237b37aafe1a6d558b8b91fa2fa0f26933141
Md Mahafujur Rahaman Masud <masudhossainmbs129@gmail.com>
2022-10-11 12:40:47 +0600

    footer component added

--- src/LayOut/Main.js
+++ src/LayOut/Main.js
@@ -1,6 +1,7 @@
 import React, { createContext } from 'react';
 import { Outlet, useLoaderData } from 'react-router-dom';
-import Header from '../components/Header/Header';
+import Header from '../components/Header/Header'; 
+import Footer from '../components/Footer/Footer'; 
 
 
 export const QuizContext = createContext([])
@@ -11,6 +12,7 @@ const Main = () => {
         <QuizContext.Provider value={getLoaderData}>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </QuizContext.Provider>
     );
 };
